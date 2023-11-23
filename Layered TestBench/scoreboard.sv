126c9d9a53ae5c09cf3b33903ef4d8d5b3b39275
ShashidharReddy01 <142148810+ShashidharReddy01@users.noreply.github.com>
2023-11-23 15:50:16 +0530

    Update scoreboard.sv

--- Layered TestBench/scoreboard.sv
+++ Layered TestBench/scoreboard.sv
@@ -1,33 +1,33 @@
 class scoreboard;
 
-	virtual parkingsystem_intf intf;
-	mailbox gen2bfm;
-	int no_transactions;
-	
-	function new(virtual debounce_intf intf ,mailbox gen2bfm);
-	this.intf=intf;
-	this.gen2bfm=gen2bfm;
-	endfunction
-	
-	task reset;
-		wait(intf.reset);
-		$display("Resetting is on");
-		intf.bfm_cb.red_led<=0;
-		//intf.bfm_cb.green_led<=0;
-		wait(!intf.reset);
-		$display("Reset done");
-	endtask
-	
-	task main;
-		forever begin
-		transaction trans;
-		gen2bfm.get(trans);
-		$display("Transaction no=%0d",no_transactions);
-		intf.bfm_cb.red_led<=trans.red_led;
-		repeat(2)@(posedge intf.clk);
-		trans.green_led=intf.bfm_cb.green_led;
-		trans.display();
-		no_transactions++;
-		end 
-	endtask
+   virtual parkingsystem_intf intf;
+   mailbox gen2bfm;
+   int no_transactions;
+   
+   function new(virtual parkingsystem_intf intf ,mailbox gen2bfm);
+   this.intf=intf;
+   this.gen2bfm=gen2bfm;
+   endfunction
+   
+   task reset;
+   	wait(intf.reset);
+   	$display("Resetting is on");
+   	intf.bfm_cb.RED_LED<=0;
+   	//intf.bfm_cb.GREEN_LED<=0;
+   	wait(!intf.reset);
+   	$display("Reset done");
+   endtask
+   
+   task main;
+   	forever begin
+   	transaction trans;
+   	gen2bfm.get(trans);
+   	$display("Transaction no=%0d",no_transactions);
+   	intf.bfm_cb.RED_LED<=trans.RED_LED;
+   	repeat(2)@(posedge intf.clk);
+   	trans.GREEN_LED=intf.bfm_cb.GREEN_LED;
+   	trans.display();
+   	no_transactions++;
+   	end 
+   endtask
 endclass 
